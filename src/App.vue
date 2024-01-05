51d12b32c45a2aa18eee0358f8dc9e8cba2dec70
jynxio <jinxiaomatrix@gmail.com>
2024-01-06 03:07:30 +0800

    Push

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
 	<div :class="$style.wrapper">
+		<section :class="$style.formula" v-html="html" />
+		<hr :class="$style.fence" />
 		<section :class="$style.control">
 			<div :class="$style.option">
 				<span :class="$style.text">clamp of x:</span>
@@ -18,7 +20,6 @@
 				<span :class="$style.input"><Toggle /></span>
 			</div>
 		</section>
-		<section :class="$style.formula"></section>
 	</div>
 </template>
 
@@ -26,21 +27,46 @@
 import Ratio from '@/components/Ratio.vue';
 import Input from '@/components/Input.vue';
 import Toggle from '@/components/Toggle.vue';
+import { codeToHtml } from 'shikiji';
+import { ref, onBeforeMount } from 'vue';
+
+const html = ref('');
+const code = `
+.wrapper {
+    font-size: clamp(2rem, 3vw + 2rem, 3rem);
+}
+`;
+
+onBeforeMount(async () => (html.value = await codeToHtml(code.trim(), { lang: 'css', theme: 'nord' })));
 </script>
 
 <style module>
 .wrapper {
-	block-size: 100vh;
-	block-size: 100dvh;
-	inline-size: 100vw;
-	inline-size: 100dvw;
-	background-color: #09090b;
+	display: flex;
+	gap: 1rem;
+	flex-direction: column;
+	align-items: center;
+	position: absolute;
+	inset: 0;
+	block-size: fit-content;
+	inline-size: fit-content;
+	margin: auto;
 }
 
-.control,
+.fence {
+	all: initial;
+	block-size: 1px;
+	inline-size: 100%;
+	background-color: #27272a;
+}
+
 .formula {
-	border-radius: 4px;
-	border: 1px solid #27272a;
+	> pre {
+		padding: 1rem;
+		border-radius: 4px;
+		border: 1px solid #27272a;
+		background-color: #18181b !important;
+	}
 }
 
 .control {
